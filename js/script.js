8d816e1eee40ca3835840423736acfe537743720
Jeroen Verfallie <me@jeroenverfallie.com>
2012-08-19 15:54:19 +0200

    Changes.

--- js/script.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-	
-	$.getJSON("http://search.twitter.com/search.json?callback=?&q=%23workinprogress%20from%3Ajeroenverfallie&count=1", function(data) {
-		$("#current span").html(((data['results'][0].text).replace('#workinprogress','')).toUpperCase());
-	});
-
-});
